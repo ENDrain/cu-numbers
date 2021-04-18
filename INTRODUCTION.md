37711b4c7c265cb5efcae7ecf23bfc5483546858
Andrey Shur <amshoor@gmail.com>
2021-04-18 19:22:41 +0300

    Add plain mode r/w Default to delim mode.
    Refactoring

--- INTRODUCTION.md
+++ INTRODUCTION.md
@@ -1,53 +1,76 @@
-# NUMBERS IN CHURCH SLAVONIC SCRIPT
+# CYRILLIC NUMERAL SYSTEM
 
 ## 1. Numerals
+Church numeral system (*further CU*) has individual letters assigned to represent numbers from 1 to 9 in registries from digits to hundreds, for a total of 27 numerals. There's no zero numeral.
 
-Church Slavonic script (*further CU*) has individual numerals to represent numbers from 1 to 9, each round ten and each round hundred, for a total of 27 numerals. There's no zero digit.
-
-CU | Arabic | CU | Arabic | CU | Arabic
---- | --- | --- | --- | --- | ---
-а҃ | 1 | і҃ | 10 | р҃ | 100
-в҃ | 2 | к҃ | 20 | с҃ | 200
-г҃ | 3 | л҃ | 30 | т҃ | 300
-д҃ | 4 | м҃ | 40 | у҃ | 400
-є҃ | 5 | н҃ | 50 | ф҃ | 500
-ѕ҃ | 6 | ѯ҃ | 60 | х҃ | 600
-з҃ | 7 | ѻ҃ | 70 | ѱ҃ | 700
-и҃ | 8 | п҃ | 80 | ѿ҃ | 800
-ѳ҃ | 9 | ч҃ | 90 | ц҃ | 900
+CU|Arabic|CU|Arabic|CU|Arabic
+---|---|---|---|---|---
+а҃|1|і҃|10|р҃|100
+в҃|2|к҃|20|с҃|200
+г҃|3|л҃|30|т҃|300
+д҃|4|м҃|40|у҃|400
+є҃|5|н҃|50|ф҃|500
+ѕ҃|6|ѯ҃|60|х҃|600
+з҃|7|ѻ҃|70|ѱ҃|700
+и҃|8|п҃|80|ѿ҃|800
+ѳ҃|9|ч҃|90|ц҃|900
 
 ## 2. Basic numbers
-
-A number `x < 1000` in CU is represented by a succession of 1 to 3 CU digits that may contain a hundreds digit, a tens digit and a proper digit in that order. Digits representing numbers 11-19 are swapped places, wheter or not a hundreds digit is present.
-
-For the purpose of this program, this is to be referenced as a "hundred group".
+A number `x < 1000` in CU is represented by appropriate registry numerals in descending registry order, except for numbers 11-19, where numerals are swapped.
 
 **Examples:**
-CU | Arabic
----|---
-а҃  | 1
-і҃ | 10
-р҃ | 100
-ра҃і | 111 - note digit swapping
-рк҃а | 121
+CU|Arabic|CU|Arabic|CU|Arabic
+---|---|---|---|---|---
+а҃|1|і҃|10|р҃|100
+|||||р҃а|101
+|||||р҃і|110
+|||а҃і|11|ра҃і|111
+|||к҃а|21|рк҃а|121
 
-## 3. Thousands
-A number `x >= 1000` in CU is prepended with one or more `҂` "thousand" signs. For each "thousand" sign before a number, the number has to be mutiplied by one thousand.
-
-## 4. Number building
-
-Altogether, a complete number in CU consists of a succession of hundred groups with "thousand" signs inbetween.
+## 3. Thousands and above
+In a number `x > 999`, the `҂` "thousand" sign is used before numerals that represent higher order of magnitude. For each thousand sign before a numeral its value is multiplied by 1000.
 
 **Examples:**
-CU | Arabic
+CU|Arabic
 ---|---
-а҃ | 1
-҂а҃ | 1000
-҂҂а҃ | 1000000
-҂҂а҂а҃а | 1001001
+҂а҃|1 000
+҂і҃|10 000
+҂р҃|100 000
+҂҂а҃|1 000 000
+
+## 4. Number building: multiple styles
+There are multiple styles of building numbers with values `x > 999`.
+
+## 4.1. "Plain" style
+In "plain" style, "thousand" signs are prepended to each numeral in higher registries, so that only the numeral following is multiplied.
+
+In this style, numerals representing numbers 11-19 are only swapped unless "thousand"-marked.
+
+***Examples:**
+CU|Arabic
+---|---
+҂р҂і҂а҂ра҃і|111 111
+
+## 4.2 "Delimeter" style
+In "delimeter" style, "thousand" signs are prepended to each group of numerals that form a basic number, so that the whole basic number following is multiplied.
+
+Unlike in "plain", in this style numerals representing numbers 11-19 are swapped on each occurence.
+
+**Examples:**
+CU|Arabic
+---|---
+҂раіра҃і|111 111
+
+## 4.3 "Circled" style
+In "circled" style, a special set of encircling markers is used to denote each higher order of magnitude from ten thousands up to billions.
+
+**Examples:**
+CU|ARABIC
+---|---
+а&#1160; а&#8413; ҂ара҃і|111 111
 
 ## 5. Decoration
-Finally, there is a `҃` "titlo" superscript sign that's obligatory to CU numbers. "Titlo" is placed above the 2nd-from-last digit if it exists and is not thousand-marked, otherwise above the last digit.
+Finally, there is a `҃`&nbsp; "titlo" superscript sign that's obligatory to CU numbers. "Titlo" is placed above the 2nd-from-last digit if it exists and is not thousand-marked, otherwise above the last digit.
     
 **Examples:**
 
--- tests/test_cunumbers.py
+++ tests/test_cunumbers.py
@@ -1,84 +1,117 @@
-import unittest, cunumbers
-_to_cu = cunumbers.to_cu
-_to_arab = cunumbers.to_arab
+import unittest
+from cunumbers import *
 
-class ToCUTestCase(unittest.TestCase):
+class ToCUPlainTestCase(unittest.TestCase):
     def test_to_cu_digits(self):
-        self.assertEqual(_to_cu(1), "а҃")
-        self.assertEqual(_to_cu(9), "ѳ҃")
+        self.assertEqual(to_cu(1), "а҃")
+        self.assertEqual(to_cu(9), "ѳ҃")
     
     def test_to_cu_tens(self):
-        self.assertEqual(_to_cu(10), "і҃")
-        self.assertEqual(_to_cu(18), "и҃і")
-        self.assertEqual(_to_cu(22), "к҃в")
+        self.assertEqual(to_cu(10), "і҃")
+        self.assertEqual(to_cu(18), "и҃і")
+        self.assertEqual(to_cu(22), "к҃в")
 
     def test_to_cu_hundreds(self):
-        self.assertEqual(_to_cu(100), "р҃")
-        self.assertEqual(_to_cu(207), "с҃з")
-        self.assertEqual(_to_cu(333), "тл҃г")
+        self.assertEqual(to_cu(100), "р҃")
+        self.assertEqual(to_cu(207), "с҃з")
+        self.assertEqual(to_cu(333), "тл҃г")
 
     def test_to_cu_thousand(self):
-        self.assertEqual(_to_cu(1000), "҂а҃")
-        self.assertEqual(_to_cu(1006), "҂а҃ѕ")
-        self.assertEqual(_to_cu(1015), "҂ає҃і")
-        self.assertEqual(_to_cu(1444), "҂аум҃д")
+        self.assertEqual(to_cu(1000), "҂а҃")
+        self.assertEqual(to_cu(1006), "҂а҃ѕ")
+        self.assertEqual(to_cu(1015), "҂ає҃і")
+        self.assertEqual(to_cu(1444), "҂аум҃д")
+        self.assertEqual(to_cu(11000, CU_PLAIN), "҂і҂а҃")
 
     def test_to_cu_big(self):
-        self.assertEqual(_to_cu(10001010001), "҂҂҂і҂҂а҂і҃а")
-        self.assertEqual(_to_cu(50000000000), "҂҂҂н҃")
-        self.assertEqual(_to_cu(60000070000), "҂҂҂ѯ҂ѻ҃")
+        self.assertEqual(to_cu(10001010001, CU_PLAIN), "҂҂҂і҂҂а҂і҃а")
+        self.assertEqual(to_cu(50000000000, CU_PLAIN), "҂҂҂н҃")
+        self.assertEqual(to_cu(60000070000, CU_PLAIN), "҂҂҂ѯ҂ѻ҃")
+        self.assertEqual(to_cu(111111111, CU_PLAIN), "҂҂р҂҂і҂҂а҂р҂і҂ара҃і")
+
+class ToCUDelimTestCase(unittest.TestCase):
+
+    def test_to_cu_delim_digits(self):
+        self.assertEqual(to_cu(1, CU_DELIM), "а҃")
+    
+    def test_to_cu_delim_tens(self):
+        self.assertEqual(to_cu(18, CU_DELIM), "и҃і")
+
+    def test_to_cu_delim_hundreds(self):
+        self.assertEqual(to_cu(833, CU_DELIM), "ѿл҃г")
+
+    def test_to_cu__delim_thousand(self):
+        self.assertEqual(to_cu(1444, CU_DELIM), "҂аум҃д")
+        self.assertEqual(to_cu(11000), "҂а҃і")
+
+    def test_to_cu_delim_big(self):
+        self.assertEqual(to_cu(10001010001), "҂҂҂і҂҂а҂і҃а")
+        self.assertEqual(to_cu(50000000000), "҂҂҂н҃")
+        self.assertEqual(to_cu(60000070000), "҂҂҂ѯ҂ѻ҃")
+        self.assertEqual(to_cu(111111111), "҂҂раі҂раіра҃і")
+
+class ToCUFlagsTestCase(unittest.TestCase):
+    def test_to_cu_notitlo(self):
+        self.assertEqual(to_cu(1, CU_NOTITLO), "а")
+        self.assertEqual(to_cu(11000, CU_PLAIN + CU_NOTITLO), "҂і҂а")
         
-class ToArabTestCase(unittest.TestCase):
+class ToArabDelimTestCase(unittest.TestCase):
     def test_to_arab_digits(self):
-        self.assertEqual(1, _to_arab("а҃"))
-        self.assertEqual(9, _to_arab("ѳ"))
+        self.assertEqual(1, to_arab("а҃"))
+        self.assertEqual(9, to_arab("ѳ"))
 
     def test_to_arab_tens(self):
-        self.assertEqual(10, _to_arab("і҃"))
-        self.assertEqual(18, _to_arab("и҃і"))
-        self.assertEqual(22, _to_arab("к҃в"))
+        self.assertEqual(10, to_arab("і҃"))
+        self.assertEqual(18, to_arab("и҃і"))
+        self.assertEqual(22, to_arab("к҃в"))
 
     def test_to_arab_hundreds(self):
-        self.assertEqual(100, _to_arab("р҃"))
-        self.assertEqual(207, _to_arab("с҃з"))
-        self.assertEqual(333, _to_arab("тл҃г"))
+        self.assertEqual(100, to_arab("р҃"))
+        self.assertEqual(207, to_arab("с҃з"))
+        self.assertEqual(333, to_arab("тл҃г"))
 
     def test_to_arab_thousands(self):
-        self.assertEqual(1000, _to_arab("҂а҃"))
-        self.assertEqual(1006, _to_arab("҂а҃ѕ"))
-        self.assertEqual(1015, _to_arab("҂ає҃і"))
-        self.assertEqual(1444, _to_arab("҂аум҃д"))
+        self.assertEqual(1000, to_arab("҂а҃"))
+        self.assertEqual(1006, to_arab("҂а҃ѕ"))
+        self.assertEqual(1015, to_arab("҂ає҃і"))
+        self.assertEqual(1444, to_arab("҂аум҃д"))
 
     def test_to_arab_big(self):
-        self.assertEqual(10001010001, _to_arab("҂҂҂і҂҂а҂і҃а"))
-        self.assertEqual(50000000000, _to_arab("҂҂҂н҃"))
-        self.assertEqual(60000070000, _to_arab("҂҂҂ѯ҂ѻ҃"))
+        self.assertEqual(10001010001, to_arab("҂҂҂і҂҂а҂і҃а"))
+        self.assertEqual(50000000000, to_arab("҂҂҂н҃"))
+        self.assertEqual(60000070000, to_arab("҂҂҂ѯ҂ѻ҃"))
 
     def test_to_arab_no_tsnd(self):
-        self.assertEqual(80500690700, _to_arab("пфхч҃ѱ"))
+        self.assertEqual(80500690700, to_arab("пфхч҃ѱ"))
 
-    def test_to_arab_no_titlo(self):
-        self.assertEqual(1, _to_arab("а"))
+    def test_to_arab_notitlo(self):
+        self.assertEqual(1, to_arab("а"))
 
     def test_to_arab_spaced(self):
-        self.assertEqual(1, _to_arab("а҃ "))
+        self.assertEqual(1, to_arab("а҃ "))
     
     def test_to_arab_uppercase(self):
-        self.assertEqual(1, _to_arab("А҃"))
+        self.assertEqual(1, to_arab("А҃"))
 
     def test_to_arab_mixed(self):
-        self.assertEqual(2021, _to_arab(" вКА"))
+        self.assertEqual(2021, to_arab(" вКА"))
+
+class ToArabPlainTestCase(unittest.TestCase):
+    def test_to_arab_plain_big(self):
+        self.assertEqual(11000, to_arab("҂і҂а"))
+        self.assertEqual(111111111, to_arab("҂҂р҂҂і҂҂а҂р҂і҂ара҃і"))
 
 class ErrorTestCase(unittest.TestCase):
     def test_to_cu_error(self):
-        self.assertRaises(TypeError, _to_cu, "String")
-        self.assertRaises(TypeError, _to_cu, 9.75)
-        self.assertRaises(ValueError, _to_cu, 0)
-        self.assertRaises(ValueError, _to_cu, -69)
+        self.assertRaises(TypeError, to_cu, "String")
+        self.assertRaises(TypeError, to_cu, 9.75)
+        self.assertRaises(ValueError, to_cu, 0)
+        self.assertRaises(ValueError, to_cu, -69)
 
     def test_to_arab_error(self):
-        self.assertRaises(TypeError, _to_arab, 420)
-        self.assertRaises(ValueError, _to_arab, "A113")
+        self.assertRaises(TypeError, to_arab, 420)
+        self.assertRaises(ValueError, to_arab, "")
+        self.assertRaises(ValueError, to_arab, "A113")
 
 if __name__ == '__main__':
     unittest.main()
